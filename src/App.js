bf4f33dd80893ed28526d46f5de16dc1e5da1b20
Divye <djain94314@gmail.com>
2023-03-29 22:16:27 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const [prompt, setPrompt] = useState('');
+  const [response, setResponse] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios
+      .post("http://localhost:5555/chat", { prompt })
+      .then((res) => {
+        setResponse(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="wrapper">
+      
+      <form onSubmit={handleSubmit}>
+        
+        <input
+          type="text"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          placeholder="Ask anything... :)"
+        />
+        <button type="submit">Ask</button>
+      </form>
+      <p className="response-area">
+        {loading ? 'loading...' : response}
+      </p>
+      <div className="footer">divye</div>
+</div>
   );
 }
 
